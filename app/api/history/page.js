7b0dd15298c588450bfd6818778e6d1cdebe5fe2
AsmaSaleem9405 <automationninga22@gmail.com>
2026-07-23 15:00:30 +0500

    add

--- app/api/history/page.js
+++ app/api/history/page.js
@@ -5,10 +5,8 @@ export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
-  const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
     if (!supabaseUrl || !supabaseKey) {
       throw new Error("Supabase environment variables are missing");
